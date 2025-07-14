2cb8f50055554d0b1102f2de92be2519397e2423
desivar <jillian_desire@yahoo.es>
2025-07-14 11:09:28 -0600

    upadted utilities/index

--- utilities/index.js
+++ utilities/index.js
@@ -4,7 +4,7 @@ const Util = {}
 /* **************************************
  * Constructs the nav HTML unordered list
  ************************************** */
-Util.getNav = async function (req, res, next) {
+Util.getNav = async function () { 
   let data = await invModel.getClassifications()
   let list = "<ul>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
@@ -31,27 +31,27 @@ Util.buildClassificationGrid = async function(data){
   let grid
   if(data.length > 0){
     grid = '<ul id="inv-display">'
-    data.forEach(vehicle => { 
+    data.forEach(vehicle => {
       grid += '<li>'
-        grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-        + 'details"><img src="' + vehicle.inv_thumbnail 
-        +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+        grid +=   '<a href="../../inv/detail/'+ vehicle.inv_id
+        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model
+        + 'details"><img src="' + vehicle.inv_thumbnail
+        +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model
         +' on CSE Motors"></a>'
         grid += '<div class="namePrice">'
           grid += '<hr>'
           grid += '<h2>'
-            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View '
+            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">'
             + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
           grid += '</h2>'
-          grid += '<span>$' 
+          grid += '<span>$'
           + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
         grid += '</div>'
       grid += '</li>'
     })
     grid += '</ul>'
-  } else { 
+  } else {
     grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
   return grid
@@ -73,7 +73,7 @@ Util.buildInventoryDisplay = async function(data){
   if(data.length > 0){
     grid = '<div id="inv-item-detail">'
       grid += '<div class="detail-img">'
-        grid +=  '<img src="' + d.inv_image 
+        grid +=   '<img src="' + d.inv_image
           + '" alt="Image of ' + d.inv_year + " " + d.inv_make + " " + d.inv_model
           + ' on CSE Motors">'
       grid += '</div>'
@@ -85,7 +85,7 @@ Util.buildInventoryDisplay = async function(data){
         grid += '<p><span>Miles: </span>' + d.inv_miles.toLocaleString('en-US') + '</p>'
       grid += '</div>'
     grid += '</div>'
-  } else { 
+  } else {
     grid += '<p class="notice">Sorry, no matching vehicle could be found.</p>'
   }
   return grid
@@ -101,7 +101,7 @@ Util.buildErrorPage = async function(){
 
 /* ****************************************
  * Middleware For Handling Errors
- * Wrap other function in this for 
+ * Wrap other function in this for
  * General Error Handling
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
