3e79083fe07d60491664fb18252eefd4918ad93a
desivar <jillian_desire@yahoo.es>
2025-07-29 17:35:34 -0600

    update accountRoute.js

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,20 +1,29 @@
 // Needed Resources 
-// Needed Resources (ONE TIME ONLY)
 const express = require("express")
-const router = new express.Router()
+const router = new express.Router() 
 const accountCont = require("../controllers/accountController")
-const utilities = require("../utilities") // Assuming this is correct path
-const regValidate = require("../utilities/account-validation")
-
-// All your route definitions go here, using the 'router' declared above
+const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountCont.buildLogin));
-
 // Route to build register view
 router.get("/register", utilities.handleErrors(accountCont.buildRegister));
+// Post new account information
+router.post(
+    '/register', 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountCont.registerAccount)
+)
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  )
 
-// ... other routes using this 'router' instance ...
-
-// And finally, export the router so your main app can use it
 module.exports = router;
