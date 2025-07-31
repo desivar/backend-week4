d5a5a2b846f632d631fb4353ec47f648cac00301
desivar <jillian_desire@yahoo.es>
2025-07-31 14:58:13 -0600

    s/index.js

--- utilities/index.js
+++ utilities/index.js
@@ -5,9 +5,20 @@ const invModel = require("../models/inventory-model")
  * ************************ */
 Util.buildClassificationList = async function (classification_id = null) {
   let data = await invModel.getClassifications()
-  // ... (rest of your buildClassificationList code)
+  let classificationList = '<select name="classification_id" id="classificationList">'
+  classificationList += "<option>Choose a Classification</option>"
+  data.rows.forEach((row) => {
+    classificationList += `<option value="${row.classification_id}"`
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected "
+    }
+    classificationList += `>${row.classification_name}</option>`
+  })
+  classificationList += "</select>"
   return classificationList
-
 }
 /* ****************************************
  * Builds the classification grid
