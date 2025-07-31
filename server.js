53901c7e0cb2666526a19b543fb97d58c1df714a
desivar <jillian_desire@yahoo.es>
2025-07-31 09:22:10 -0600

    fix typo server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 /* ***********************
  * Require Statements
  *************************/
-const env = require("dotenv").config()
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const static = require("./routes/static")
