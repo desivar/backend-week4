4eabfb0a3042b1d8854996655e4e8a5ac12af72d
desivar <jillian_desire@yahoo.es>
2025-07-31 14:36:48 -0600

    /invController.js

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,107 +1,90 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
+const invCont = {};
 
-const invCont = {}
-
-/* ***************************
- *  Build inventory by classification view
+/* *************************** 
+ *  Build inventory by classification view 
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-    const classification_id = req.params.classificationId
-    const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildClassificationGrid(data)
-    let nav = await utilities.getNav()
-    const className = data[0].classification_name
+    const classification_id = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(classification_id);
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className = data[0].classification_name;
     res.render("./inventory/classification", {
-      title: className + " Vehicles",
-      nav,
-      grid,
-    })
-}
+        title: className + " Vehicles",
+        nav,
+        grid,
+    });
+};
 
-/* ***************************
- *  Build inventory item detail view
+/* *************************** 
+ *  Build inventory item detail view 
  * ************************** */
 invCont.buildByInventoryId = async function (req, res, next) {
-    const inv_id = req.params.inventoryId
-    const data = await invModel.getInventoryByInventoryId(inv_id)
-    const grid = await utilities.buildInventoryDisplay(data)
-    let nav = await utilities.getNav()
-    let d = data[0]
-    const className = d.inv_year + " " + d.inv_make + " " + d.inv_model
+    const inv_id = req.params.inventoryId;
+    const data = await invModel.getInventoryByInventoryId(inv_id);
+    const grid = await utilities.buildInventoryDisplay(data);
+    let nav = await utilities.getNav();
+    let d = data[0];
+    const className = d.inv_year + " " + d.inv_make + " " + d.inv_model;
     res.render("./inventory/detail", {
-      title: className,
-      nav,
-      grid,
-    })
-}
+        title: className,
+        nav,
+        grid,
+    });
+};
 
-/* *****************************
- *  Build inventory management page
- * **************************** */
 /* *****************************
  *  Build inventory management page
  * **************************** */
 invCont.buildInvManagement = async function (req, res, next) {
-  let nav = await utilities.getNav()
-  const classificationSelect = await utilities.buildClassificationList()
-  res.render("./inventory/management", {
-    title: "Inventory Management",
-    nav,
-    errors: null,
-    classificationSelect,
-  })
-}
+    let nav = await utilities.getNav();
+    const classSelect = await utilities.buildClassificationList();
+    res.render("./inventory/management", {
+        title: "Inventory Management",
+        nav,
+        errors: null,
+        classSelect,
+    });
+};
 
-/* *****************************
- *  Build add classification page
+/* ***************************** 
+ *  Build add classification page 
  * **************************** */
 invCont.buildAddClassification = async function (req, res, next) {
-  let nav = await utilities.getNav()
-  res.render("./inventory/add-classification", {
-    title: "Add Classification",
-    nav,
-    errors: null,
-  })
-}
+    let nav = await utilities.getNav();
+    res.render("./inventory/add-classification", {
+        title: "Add Classification",
+        nav,
+        errors: null,
+    });
+};
 
-/* ****************************************
-*  Process Add Classification
-* *************************************** */
+/* *****************************************  
+ *  Process Add Classification 
+ * *************************************** */
 invCont.processAddNewClassification = async function (req, res, next) {
-  const { classification_name } = req.body
-  const result = await invModel.addNewClassification(classification_name)
+    const { classification_name } = req.body;
+    const result = await invModel.addNewClassification(classification_name);
+    console.log("*********** :: " + JSON.stringify(result));
+    if (result) {
+        let nav = await utilities.getNav();
+        req.flash('notice', `${classification_name} successfully added.`);
+        res.status(201).render("./inventory/add-classification", {
+            title: "Add Classification",
+            nav,
+            errors: [],
+        });
+    } else {
+        let nav = await utilities.getNav();
+        req.flash("notice", "Something went wrong.");
+        res.status(501).render("./inventory/add-classification", {
+            title: "Add Classification",
+            nav,
+            errors: [],
+        });
+    }
+};
 
-  console.log("*********** :: " + JSON.stringify(result))
-  if (result) {
-    let nav = await utilities.getNav()
-    req.flash('notice', `${classification_name} successfully added.`)
-    res.status(201).render("./inventory/add-classification", {
-      title: "Add Classification",
-      nav,
-      errors: [],
-    })
-  } else {
-    let nav = await utilities.getNav()
-    req.flash("notice", "Something went wrong.")
-    res.status(501).render("./inventory/add-classification", {
-      title: "Add Classification",
-      nav,
-      errors: [],
-    })
-  }
-}
-
-/* *****************************
-* Build inventory management page
-* **************************** */
-invCont.buildInvManagement = async function (req, res, next) {
-  let nav = await utilities.getNav()
-  let classSelect = await utilities.buildClassificationList()
-  res.render("./inventory/management", {
-    title: "Inventory Management",
-    nav,
-    errors: null,
-    classSelect,
-  })
-}
+module.exports = invCont;
