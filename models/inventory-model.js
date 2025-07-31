8ddc0e4ff0d9a226b65b8ed4eb08346c10ba6be9
desivar <jillian_desire@yahoo.es>
2025-07-31 14:36:21 -0600

    inventory-model.js

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -7,7 +7,7 @@ async function getClassifications() {
     try {
         return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
     } catch (error) {
-        console.error("***** getClassifications error: " + error)
+        return error.message
     }
 }
 
