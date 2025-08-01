290d1ca1eedc52d633902c6206775284d9935505
desivar <jillian_desire@yahoo.es>
2025-08-01 12:37:12 -0600

    updated  const cookieParser = require("cookie-parser")

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const utilities = require("./utilities")
 const session = require("express-session")
 const pool = require('./database/')
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /* ***********************
 
@@ -67,7 +68,7 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
+app.use(cookieParser())
 
 /* ***********************
 
